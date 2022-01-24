3057ff46fc4c53d3c7cf7b59b4c0dc450e068860
Rafael Couto <rafaelgcouto@gmail.com>
2022-01-24 23:19:25 +0000

    Adds script that converts W to kWh on Home Assistant

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # W2kWh-home-assistant
-Python script that reads Watts from Home Assistant sensors and tracks sensor in kWh on MQTT topic.
+## _Generate kWh data from Watt data from your existing devices_
+
+This project aims to create kWh data from sensors that don't generate this data but only output Watt information. The wattage information is retrieved from Home Assistant, processed and written back to Home Assistant in kWh.
+
+When you run this script, the software will read Watt data from Home Assistant and it will create a new sensor on Home Assistant called [name_of_your_sensor]_energy (eg. sensor.ac_kitchen_power -> sensor.ac_kitchen_power_energy) where new kWh data is going to be written. After you'll be able to integrate this sensor to the Energy panel on Home Assistant.
+
+DISCLAIMER: The generated energy information might not be 100% accurate, please use at own discretion!
+
+## Tech
+
+This project was developed using Python 3.8.9 and uses a couple external libraries
+- Requests - to make requests to Home Assistant
+- Decouple - to read from .env files
+
+## Setup
+
+It is recomended that you have Python 3 installed in your machine.
+
+First of all, clone this repository to your machine.
+
+```sh
+git clone https://github.com/rgcouto/W2kWh-home-assistant.git
+```
+
+Install the dependencies
+
+```sh
+cd W2kWh-home-assistant
+pip3 install -r requirements.txt
+```
+
+Configure the environment variables. The project includes an example enviroment variables file which you can use. After copying it please fill out the required fields
+
+```sh
+cp .env_example .env
+vi||nano .env
+```
+
+Example below:
+
+```sh
+#
+# HOME ASSISTANT CONFIGURATIONS
+#
+HA_TOKEN=eyJ0eXaSDQV1QiLCJhbGciOiJIUzI1NiJ9.AasdaSDqwEqDWeFeGtrhWEFShYjTYRhGASDfaGwER.SPSON3KUHmEhSyWeh7e0vIcz9110AE5wZtzqFQ6giA4
+HA_PROTOCOL=http
+HA_HOST=192.168.4.100
+HA_PORT=8123
+
+#
+# MQTT SERVER CONFIGURATIONS
+#
+MQTT_CLIENT_ID=sensor_data
+MQTT_USERNAME=mqtt_username
+MQTT_PASSWORD=mqtt_password
+MQTT_HOST=192.168.4.100
+MQTT_PORT=1883
+
+#
+# SENSORS LIST
+#   USAGE: entity_id_1,friendly_name;entity_id_2,friendly_name (friendly name without spaces)
+ENTITIES=ac_living_room_power,AC-Living-Room;ac_masterbedroom_power,AC-Master-Bedroom
+
+```
+
+NOTE: Please notice that the provided credentials are fake and should be replaced by proper and existing ones.
+
+When everything is correctly setup, start the script
+```sh
+python3 main.py 2>&1 &
+```
+
+
+## Development
+
+Want to contribute? Great! Feel free to submit a Pull Request!
+
+
+## License
+MIT
---
+++ main.py
@@ -0,0 +1,92 @@
+import json
+import os
+import time
+from requests import get, post
+from decouple import config
+
+# Loads and sets Home Assistant configurations
+ha_token = config('HA_TOKEN')
+ha_protocol = config('HA_PROTOCOL')
+ha_host = config('HA_HOST')
+ha_port = config('HA_PORT')
+
+# Loads entities
+entities = config('ENTITIES').split(';')
+
+# Creates folder structure
+sensor_data = "sensor_data"
+if not os.path.exists(sensor_data):
+    os.makedirs(sensor_data)
+
+# Keeps application up and running
+while True:
+
+    # Loops through entities
+    for entity in entities:
+
+        # entity_id,unit
+        entity_id = entity.split(',')[0]
+        friendly_name = entity.split(',')[1]
+
+        # Gets current Wattage from sensor from Home Assistant
+        url = ha_protocol + "://" + ha_host + ":" + ha_port + "/api/states/" + entity_id
+        headers = {
+            "Authorization": "Bearer " + ha_token,
+            "content-type": "application/json",
+        }
+        response = get(url, headers=headers)
+        response_json = json.loads(response.text)
+
+        # If sensor doesn't exist skips
+        if response.status_code == 404:
+            continue
+
+        # If sensor is new
+        if not os.path.isfile(sensor_data + "/" + entity_id):
+            last_reading = [0, time.time()]
+
+            dt = 1
+            average = float(response_json['state'])
+            energy_ws = average * dt
+
+            file = open(sensor_data + "/" + entity_id, "a")
+            file.write("%s;%s;%s\n" % (energy_ws, float(response_json['state']), time.time()))
+            file.close()
+
+        # If sensor already exists
+        else:
+            with open(sensor_data + "/" + entity_id) as f:
+                last_energy = f.readlines()[0].split(";")
+
+            # TODO: Take care of long power outage periods
+            # Have to take into consideration the possibility of having the device turned off for a long period of time.
+            # What happens with the last value of W? Might be misleading. But is it significant?
+
+            dt = time.time() - float(last_energy[2])
+            average = (float(response_json['state']) + float(last_energy[1])) / 2
+            energy_ws = float(last_energy[0]) + average * dt
+
+            file = open(sensor_data + "/" + entity_id, "w")
+            file.write("%s;%s;%s\n" % (energy_ws, float(response_json['state']), time.time()))
+            file.close()
+
+            # Converts Ws to kWh
+            energy_kwh = energy_ws/3600/1000
+
+            # Posts to Home Assistant the kWh value of the device so it can be read by the Energy Panel
+            device_body = {
+                "state": energy_kwh,
+                "attributes": {
+                    "state_class": "total_increasing",
+                    "unit_of_measurement": "kWh",
+                    "device_class": "energy",
+                    "friendly_name": friendly_name
+                }
+            }
+            response = post(url + "_energy", headers=headers, data=json.dumps(device_body))
+            if response.status_code != 200:
+                print("ERROR SUBMITTING ENERGY OF " + entity_id)
+                print("WILL EXIT!")
+                exit()
+
+    time.sleep(5)
